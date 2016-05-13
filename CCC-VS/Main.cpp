e078edfe1aaf33d500dd9af9797fc27e6c860c0a
Rndphoenx <MalteH2012@hotmail.de>
2016-05-13 21:54:51 +0200

    Added Key, printed timestamp

--- CCC-VS/Main.cpp
+++ CCC-VS/Main.cpp
@@ -6,16 +6,19 @@ void output(const char logpath[100]);
 
 struct User {
 	char name[50];
-	time_t lastlogin;
+	time_t lastloginTimestamp;
 };
 
 typedef struct User User;
 
 int main(void) {
 	User test;
-	test.lastlogin = time(NULL);
 	const char logpath[100] = "testlog.txt";
+	const char key[100] = "rB880$561k9xs0n6Zk917d59F3R7e30$2L7674F1O53aVnhI3rS*!12QG899c6I265D784U822IRy#W175628Xw5p4v274DJg8c"; //KEY IN PLAINTEXT: MUCH SECÙRE, SUCH SAFE
 	
+	time(&test.lastloginTimestamp);
+	printf("%lld\n", (long long)test.lastloginTimestamp);
+
 	output(logpath);
 
 	printf("\n\n ||END OF 0.0.2|| \n\n");
@@ -36,5 +39,4 @@ void output(const char logpath[100]) {
 			}
 		}
 		else printf("ERROR 001");
-		
 }
